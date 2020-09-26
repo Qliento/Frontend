993c922e0962162eb867555a97a971b26a549b9d
Gerasko-Vadim <vadim.gerasko99@gmail.com>
2020-09-27 00:24:08 +0600

    create block order and about us

---
+++ src/components/home/news/news.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import classes from "./news.module.css";
+
+const News =()=>{
+    return(
+        <div className={classes.blockNews}></div>
+    )
+}
+export default News;
