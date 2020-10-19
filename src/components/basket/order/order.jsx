28ab577998743b2260fd3dc812875195d97af521
Gerasko-Vadim <vadim.gerasko99@gmail.com>
2020-10-20 00:09:47 +0600

    fixed bug

--- src/components/basket/order/order.jsx
+++ src/components/basket/order/order.jsx
@@ -4,19 +4,9 @@ import classes from "./order.module.css"
 const Order=()=>{
     return(
         <div className={classes.orderCard}>
-            <div className={classes.content}>
-                <div className={classes.blockText}>
-                    <span className={classes.title}>Сумма</span>
-                    <span className={classes.price}>110 000 сом</span>
-                </div>
-                <div className={classes.blockText}>
-                    <span className={classes.title}>Итого</span>
-                    <span className={classes.price2}>110 000 сом</span>
-                </div>
-                <div className={classes.blockText }>
-                    <span className={classes.discounts }>Скидка</span>
-                    <span className={classes.priceDescounts}>0 сом </span>
-                </div>
+            <div className={classes.blockPrice}>
+                <span className={classes.titlePrice}>Итоговоя сумма</span>
+                <span className={classes.price}>110 000 сом </span>
             </div>
             <button className={classes.btn}>Оформить заказ</button>
         </div>
