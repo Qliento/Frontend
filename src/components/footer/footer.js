bd9d0a428137a367c8b72e5e7633185c7d54af16
Shama Nurlanov <esenkozhoev99@inbox.ru>
2020-12-05 23:12:45 +0600

    m

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -13,10 +13,6 @@ const Footer = () => {
     dispatch(mainData());
   }, []);
   let arrContacts = [];
-<<<<<<< HEAD
-=======
-
->>>>>>> 5fdae023206923fbf0eac931c2d42486ac39f4bd
   // if (data.сontacts) {
   //     arrContacts = [...data.сontacts.contacts];
     
